eedab26f25616a4505e507a9b6c8746ad0fe7e57
srockw <136873475+srockw@users.noreply.github.com>
2025-03-13 22:59:16 -0300

    (Grand Strategy) Fix typo

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -634,7 +634,7 @@ return {
         name = "Grand Strategy",
         text = {
           "This Joker gives {X:mult,C:white}X#1#{} Mult if",
-          "{C:attention}#2#{} unique {C:attention}enahncements{}, {C:attention}editions{},",
+          "{C:attention}#2#{} unique {C:attention}enhancements{}, {C:attention}editions{},",
           "or {C:attention}seals{} are in your full deck",
           "{C:inactive}(Currently {C:attention}#3#{C:inactive})",
         },
