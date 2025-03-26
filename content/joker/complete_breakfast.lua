f6fe6e8aa2d90ab7bdde5f66a2359d29a4361de9
OppositeWolf770 <dhoelzeman@proton.me>
2025-03-26 02:30:29 -0500

    (Complete Breakfast) Add JokerDisplay

--- content/joker/complete_breakfast.lua
+++ content/joker/complete_breakfast.lua
@@ -66,5 +66,32 @@ SMODS.Joker {
         }
       end
     end
+  end,
+
+  joker_display_def = function(JokerDisplay)
+    return {
+      text = {
+        { text = '+',                       colour = G.C.MULT },
+        { ref_table = 'card.ability.extra', ref_value = 'mult',  colour = G.C.MULT },
+        { text = ' +',                      colour = G.C.CHIPS },
+        { ref_table = 'card.ability.extra', ref_value = 'chips', colour = G.C.CHIPS },
+      },
+
+      extra = {
+        {
+          { text = '(' },
+          { ref_table = 'card.joker_display_values', ref_value = 'odds' },
+          { text = ')' },
+        }
+      },
+      extra_config = {
+        colour = G.C.GREEN,
+        scale = 0.3,
+      },
+
+      calc_function = function(card)
+        card.joker_display_values.odds = localize { type = 'variable', key = 'jdis_odds', vars = { (G.GAME and G.GAME.probabilities.normal or 1) * card.ability.extra.chance_multiplier, card.ability.extra.odds } }
+      end
+    }
   end
 }
