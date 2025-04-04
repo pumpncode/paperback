7e1b8ab11d604cfd23ab8622e75353c93fa0624f
OppositeWolf770 <dhoelzeman@proton.me>
2025-04-04 02:34:51 -0500

    (Four of Wands) Makes 2 clips (was 3)

--- content/minor_arcana/four_of_wands.lua
+++ content/minor_arcana/four_of_wands.lua
@@ -2,7 +2,7 @@ PB_UTIL.MinorArcana {
   key = 'four_of_wands',
   config = {
     paperclip = 'pink',
-    max_highlighted = 3
+    max_highlighted = 2
   },
   atlas = 'minor_arcana_atlas',
   pos = { x = 3, y = 2 },
