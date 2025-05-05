bd471d6cd94647f8c42653f560f39ae2369cfbaa
Dragokillfist <tinker198431@gmail.com>
2025-05-05 11:00:09 -0500

    Decrease the Number of Paperclips that the Three of Cups applies from 3 to 1

--- content/minor_arcana/three_of_cups.lua
+++ content/minor_arcana/three_of_cups.lua
@@ -2,7 +2,7 @@ PB_UTIL.MinorArcana {
   key = 'three_of_cups',
   config = {
     paperclip = 'black',
-    max_highlighted = 3
+    max_highlighted = 1
   },
   atlas = 'minor_arcana_atlas',
   pos = { x = 2, y = 0 },
