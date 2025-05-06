d323b1519b69bbb899bebdb21e1751ef708480ac
english5040 <ejwu@andrew.cmu.edu>
2025-05-06 00:02:27 -0400

    (River) Fix undefined behavior when checking lowest card debuff
    New code is designed to be as nice to the player as possible: any one non-debuffed lowest card works. to avoid confusion on needing to reorder hand

--- content/joker/river.lua
+++ content/joker/river.lua
@@ -27,17 +27,26 @@ SMODS.Joker {
     if context.before and context.main_eval then
       -- Check if the scoring_hand contains five cards
       if #context.scoring_hand >= 5 then
-        local lowest_chip_card = context.scoring_hand[1]
+        local lowest_chip_cards = {context.scoring_hand[1]}
+        local lowest_chip_bonus = context.scoring_hand[1]:get_chip_bonus()
 
         -- Find the lowest card in the scoring_hand
-        for k, current_card in pairs(context.scoring_hand) do
-          if current_card:get_chip_bonus() < lowest_chip_card:get_chip_bonus() then
-            lowest_chip_card = current_card
+        for i, current_card in ipairs(context.scoring_hand) do
+          if i ~= 1 then
+            if current_card:get_chip_bonus() < lowest_chip_bonus then
+              lowest_chip_cards = {current_card}
+            elseif current_card:get_chip_bonus() == lowest_chip_bonus then
+              table.insert(lowest_chip_cards, current_card)
+            end
           end
         end
 
-        -- Do not give money if the lowest card is debuffed
-        if lowest_chip_card.debuff then
+        -- Do not give money if all lowest cards are debuffed
+        local all_debuffed = true
+        for _, c in ipairs(lowest_chip_cards) do
+          if not c.debuff then all_debuffed = false; break end
+        end
+        if all_debuffed then
           return {
             message = localize('k_debuffed'),
             colour = G.C.MULT,
@@ -46,10 +55,8 @@ SMODS.Joker {
         end
 
         -- Calculate the money to give and return it
-        local lowest_chip_bonus = lowest_chip_card:get_chip_bonus()
         local money_to_give = lowest_chip_bonus > card.ability.extra.money_cap
             and card.ability.extra.money_cap or lowest_chip_bonus
-
         ease_dollars(money_to_give)
 
         return {
