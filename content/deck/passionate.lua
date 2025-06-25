0b7a48f7796a94eff924c5c5a013df591be16ede
TheSnaz <Snaz.Dev@protonmail.com>
2025-06-25 13:34:50 +0200

    (Passionate Deck) requires tags to be enabled

--- content/deck/passionate.lua
+++ content/deck/passionate.lua
@@ -1,23 +1,25 @@
-SMODS.Back {
-  key = 'passionate',
-  atlas = 'decks_atlas',
-  pos = { x = 5, y = 0 },
+if PB_UTIL.config.tags_enabled then
+  SMODS.Back {
+    key = 'passionate',
+    atlas = 'decks_atlas',
+    pos = { x = 5, y = 0 },
 
-  loc_vars = function(self, info_queue, card)
-    return {
-      vars = {
-        localize {
-          type = 'name_text',
-          set = 'Tag',
-          key = 'tag_paperback_high_risk'
+    loc_vars = function(self, info_queue, card)
+      return {
+        vars = {
+          localize {
+            type = 'name_text',
+            set = 'Tag',
+            key = 'tag_paperback_high_risk'
+          }
         }
       }
-    }
-  end,
+    end,
 
-  calculate = function(self, back, context)
-    if context.context == 'eval' and G.GAME.last_blind and G.GAME.last_blind.boss then
-      PB_UTIL.add_tag('tag_paperback_high_risk', nil, false)
+    calculate = function(self, back, context)
+      if context.context == 'eval' and G.GAME.last_blind and G.GAME.last_blind.boss then
+        PB_UTIL.add_tag('tag_paperback_high_risk', nil, false)
+      end
     end
-  end
-}
+  }
+end
