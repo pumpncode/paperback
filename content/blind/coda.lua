725461324afab6abd528a890886f6ec3b22058e5
srockw <136873475+srockw@users.noreply.github.com>
2025-07-13 03:11:22 -0300

    (Coda) Fix cards staying unmovable

--- content/blind/coda.lua
+++ content/blind/coda.lua
@@ -7,3 +7,14 @@ SMODS.Blind {
   atlas = 'music_blinds_atlas',
   pos = { y = 1 },
 }
+
+local move_ref = Moveable.drag
+function Moveable.drag(self, offset)
+  if self.is and type(self.is) == "function" and self:is(Card) then
+    if G and G.GAME and G.GAME.blind and G.GAME.blind.boss and not G.GAME.blind.disabled and G.GAME.blind.name == 'bl_paperback_coda' then
+      return
+    end
+  end
+
+  return move_ref(self, offset)
+end
