e1e64cf077de1269bb026e1d69192e356ff4e419
srockw <136873475+srockw@users.noreply.github.com>
2025-07-14 15:07:24 -0300

    (Natural) Fix talisman crash

--- content/blind/natural.lua
+++ content/blind/natural.lua
@@ -10,12 +10,12 @@ SMODS.Blind {
 
   calculate = function(self, blind, context)
     if context.before and not blind.disabled then
-      local min = math.huge
-      for i, v in pairs(G.GAME.hands) do
-        min = min < v.level and min or v.level
+      local min
+      for _, v in pairs(G.GAME.hands) do
+        min = min and (min < v.level and min or v.level) or v.level
       end
-      if G.GAME.hands[context.scoring_name].level > to_big(min) then
-        for k, v in pairs(context.scoring_hand) do
+      if min and G.GAME.hands[context.scoring_name].level > min then
+        for _, v in pairs(context.scoring_hand) do
           v:set_debuff(true)
           v.debuffed_by_blind = true
         end
