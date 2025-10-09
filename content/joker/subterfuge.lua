b45164818a2915e7acff08511de510c255818cf3
english5040 <ejwu@andrew.cmu.edu>
2025-10-09 00:43:08 -0400

    (Subterfuge) use context.destroy_card

--- content/joker/subterfuge.lua
+++ content/joker/subterfuge.lua
@@ -15,21 +15,20 @@ SMODS.Joker {
   soul_pos = nil,
 
   calculate = function(self, card, context)
-    if not context.blueprint and context.after and context.main_eval then
-      local destroyed_cards = {}
-
-      for _, v in ipairs(G.play.cards) do
-        destroyed_cards[#destroyed_cards + 1] = v
-      end
-
+    if not context.blueprint and context.destroy_card and (context.cardarea == G.play or context.cardarea == 'unscored') then
       -- Destroy all cards in first hand
       if G.GAME.current_round.hands_played == 0 then
-        SMODS.destroy_cards(destroyed_cards)
-
-        return {
-          message = localize('paperback_destroyed_ex'),
-          colour = G.C.MULT
-        }
+        if context.destroy_card == context.full_hand[#context.full_hand] then
+          return {
+            remove = true,
+            message = localize('paperback_destroyed_ex'),
+            colour = G.C.RED
+          }
+        else
+          return {
+            remove = true,
+          }
+        end
       end
     end
   end
