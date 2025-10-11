d49a2d39242337c166203866c1dc6418ff5b01e0
english5040 <ejwu@andrew.cmu.edu>
2025-10-11 00:43:48 -0400

    Wrap Broken Glasses calc in an event, better animation
    This might need more careful code than I did. 2 layers of event might be needed, and there's also the playing_card_joker_effects() call which may need to go in a different layer

--- content/ego_gift/broken_glasses.lua
+++ content/ego_gift/broken_glasses.lua
@@ -16,20 +16,28 @@ PB_UTIL.EGO_Gift {
   end,
   ego_gift_calc = function(self, card, context)
     if context.remove_playing_cards and #context.removed > 0 and #context.removed <= 2 then
-      for _, v in ipairs(context.removed) do
-        local fx = {}
-        for i = 1, 2, 1 do
-          G.playing_card = (G.playing_card or 0) + 1
-          local new_card = copy_card(v, nil, nil, G.playing_card)
-          new_card:add_to_deck()
-          G.deck.config.card_limit = G.deck.config.card_limit + 1
-          table.insert(G.playing_cards, new_card)
-          G.hand:emplace(new_card)
-          new_card:start_materialize(nil, _first_dissolve)
-          fx[#fx + 1] = new_card
+      G.E_MANAGER:add_event(Event({
+        func = function()
+          local fx = {}
+          local _first_dissolve = nil
+          for _, v in ipairs(context.removed) do
+            for i = 1, 2, 1 do
+              G.playing_card = (G.playing_card or 0) + 1
+              local new_card = copy_card(v, nil, nil, G.playing_card)
+              new_card:add_to_deck()
+              G.deck.config.card_limit = G.deck.config.card_limit + 1
+              table.insert(G.playing_cards, new_card)
+              G.hand:emplace(new_card)
+              new_card:start_materialize(nil, _first_dissolve)
+              _first_dissolve = true
+              fx[#fx + 1] = new_card
+            end
+          end
+          playing_card_joker_effects(fx)
+          return true
         end
-        playing_card_joker_effects(fx)
-      end
+      }))
+
       if PB_UTIL.chance(card, pseudoseed('broken_glasses'), nil, card.ability.odds) then
         SMODS.destroy_cards({ card })
         return {
