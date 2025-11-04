2dd54ef81bf36050df09f07424abbbf6689fd673
TheSnaz <Snaz.Dev@protonmail.com>
2025-11-04 23:06:20 +0100

    fixed (Apostle of Pentacles) being a Minor Arcana instead of a Consumable

--- content/spectrals/apostle_of_pentacles.lua
+++ content/spectrals/apostle_of_pentacles.lua
@@ -1,4 +1,4 @@
-PB_UTIL.MinorArcana {
+SMODS.Consumable {
   key = 'apostle_of_pentacles',
   config = {
     max_highlighted = 1,
@@ -13,6 +13,26 @@ PB_UTIL.MinorArcana {
   soul_rate = 0.001,
 
   paperback = {
-    requires_paperclips = true
-  }
+    requires_paperclips = true,
+  },
+
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue + 1] = PB_UTIL.paperclip_tooltip(card.ability.paperclip)
+
+    return {
+      vars = {
+        card.ability.max_highlighted
+      }
+    }
+  end,
+
+  use = function(self, card, area)
+    PB_UTIL.use_consumable_animation(card, G.hand.highlighted, function()
+      for _, v in ipairs(G.hand.highlighted) do
+        if card.ability.paperclip then
+          PB_UTIL.set_paperclip(v, card.ability.paperclip)
+        end
+      end
+    end)
+  end
 }
