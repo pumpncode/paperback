465fc67fa187d3ec97194a95b4868fa84418246b
srockw <136873475+srockw@users.noreply.github.com>
2025-11-08 13:49:39 -0300

    (Four of Swords) Allows creating Apostles if there is one in deck

--- content/minor_arcana/four_of_swords.lua
+++ content/minor_arcana/four_of_swords.lua
@@ -16,6 +16,7 @@ PB_UTIL.MinorArcana {
 
   use = function(self, card, area)
     local pool = {}
+    local allow_apostles = false
 
     for _, rank_key in ipairs(SMODS.Rank.obj_buffer) do
       local v = SMODS.Ranks[rank_key]
@@ -24,9 +25,20 @@ PB_UTIL.MinorArcana {
       end
     end
 
+    for _, playing_card in ipairs(G.playing_cards) do
+      if PB_UTIL.is_rank(playing_card, 'paperback_Apostle') then
+        allow_apostles = true
+        break
+      end
+    end
+
     PB_UTIL.use_consumable_animation(card, G.hand.highlighted, function()
       for _, v in ipairs(G.hand.highlighted) do
-        local rank = pseudorandom_element(pool, pseudoseed('four_of_swords'))
+        local rank = pseudorandom_element(pool, pseudoseed('four_of_swords'), {
+          paperback = {
+            allow_apostles = allow_apostles
+          }
+        })
         assert(SMODS.change_base(v, nil, rank.key))
       end
     end)
